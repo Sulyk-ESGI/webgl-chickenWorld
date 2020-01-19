96a21217409af48bb824ed8754712ad6cd386523
Boudacher Sylvain <sboudacher@myges.fr>
2020-01-19 18:35:39 +0100

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,460 @@
+import * as THREE from './three.module.js';
+
+import Stats from './stats.module.js';
+import { GUI } from './dat.gui.module.js';
+import { OrbitControls } from './OrbitControls.js';
+import { GLTFLoader } from './GLTFLoader.js'
+
+var container, stats;
+var camera, scene, renderer;
+
+init();
+animate();
+
+function init() {
+    // Création d'une div dans le dom, et ajout du container
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    // Création de la scène
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xCCE0FF);
+    scene.fog = new THREE.Fog( 0xcce0ff, 400, 2000 );
+
+    // Création de notre caméra
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 10000);
+    // Définition de la position de la caméra
+        camera.position.z = 500;
+        camera.position.y = 150;
+        camera.position.x = 10;
+        camera.rotation.y = 10;
+
+    setLights();
+
+    grounds();
+
+    rendered();
+
+    setOrbitControls();
+
+    setStatsModule();
+
+   loadIle();
+
+    loadHouse();
+
+    loadboat();
+
+    loadtree();
+
+    loadChiken();
+
+    for (var i = 0; i < 10; i++) {
+        loadcrystal((0+(i-0.5)),25.9,(-10+(i*0.1)));
+    }
+     
+
+    
+
+    // Evenement resize relié à la fonction onWindowResize
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+/**
+ * Fonction d'ajout de lumière
+ */
+function setLights() {
+    // Ajout d'une lumière ambiante à notre scène
+    scene.add(new THREE.AmbientLight(0x666666));
+    // Ajout d'une lumière directionnelle d'intensité 1
+    var light = new THREE.DirectionalLight(0xdfebff, 1);
+
+    /**
+     * Position de là où pointe la lumière (x, y, z)
+     * Activation des ombres pour la lumière
+     * Propriétés des ombres (width / height)
+     */
+    light.position.set(50, 200, 100);
+    light.castShadow = true;
+    light.shadow.mapSize.width = 1024;
+    light.shadow.mapSize.height = 1024;
+
+    var d = 300;
+    light.shadow.camera.left = -d;
+    light.shadow.camera.right = d;
+    light.shadow.camera.top = d;
+    light.shadow.camera.bottom = -d;
+    light.shadow.camera.far = 1000;
+    scene.add(light);
+}
+
+/**
+ * Fonction de création du sol
+ */
+function grounds() {
+    // Pré-chargement d'une texture
+    var loader = new THREE.TextureLoader();
+    // Chargement de la texture du sol
+    var groundTexture = loader.load('src/textures/water.png');
+    // Activation du mode wrap pour la répétition de la texture
+    groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+    // Paramtère de répétition de la texture
+    groundTexture.repeat.set(10,10);
+    // Gestion du flou
+    groundTexture.anisotropy = 16;
+    // Encodage avec sRGBEncoding pour une image plus nette
+    groundTexture.encoding = THREE.sRGBEncoding;
+    // Liaison material <=> texture
+    var groundMaterial = new THREE.MeshLambertMaterial({map: groundTexture});
+    // Création de notre mesh
+    var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial);
+    // Définition de la position Y de notre plateau
+    mesh.position.y = -250;
+    // Ajout d'une rotation en x
+    mesh.rotation.x = -Math.PI / 2;
+    // Activation des ombres sur le sol
+    mesh.receiveShadow = true;
+    // Ajout à la scène
+    scene.add(mesh);
+}
+
+/**
+ * Fonction de paramètrage du rendered
+ */
+function rendered() {
+    // Paramètre du rendered
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    // Définition de la taille de notre rendered par les dimensions de notre fenêtre
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Ajout des elements dom à notre container
+    container.appendChild( renderer.domElement );
+    // Encodage de la sortie avec sRGBEncoding
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    // Activation des ombres globales
+    renderer.shadowMap.enabled = true;
+}
+
+/**
+ * Fonction de paramètrage de l'Orbit Control
+ */
+function setOrbitControls() {
+    // Utilisation de l'Orbit Controls pour une navigation plus fluide
+    var controls = new OrbitControls(camera, renderer.domElement);
+    // Limitation de l'angle de rotation pour bloquer le basculement du monde
+    controls.maxPolarAngle = Math.PI * 0.5;
+    // Définition de la distance minimale et maximale à laquelle on peut aller dans notre monde
+    controls.minDistance = 50;
+    controls.maxDistance = 1000;
+}
+
+/**
+ * Fonction d'initialisation du module stats
+ */
+function setStatsModule() {
+    // Utilisation du module stats et ajout dans notre container
+    stats = new Stats();
+    container.appendChild(stats.dom);
+}
+
+/**
+ * Affichage du model 3D de la maison
+ */
+function loadIle() {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/terrain/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            gltf.scene.traverse( function ( child ) {
+
+                if ( child.isMesh ) {
+
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+
+                }
+
+            } );
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+           /* gltf.scene.rotation.y = -300;
+            gltf.scene.position.x = 500;
+            gltf.scene.position.z = -750;
+            gltf.scene.position.y = -150;*/
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded ile');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+function loadtree() {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/tree/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.rotation.y = 0;
+            gltf.scene.position.x = 1800;
+            gltf.scene.position.z = -600;
+            gltf.scene.position.y = -500;
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded tree');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+function loadChiken() {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/chicken/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene.scale.set(2,2,2); // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.rotation.y = 10;
+            gltf.scene.position.x = 10; //longeur
+            gltf.scene.position.z = 50;//profondeur
+            gltf.scene.position.y = 26; //hauteur
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded poulet');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue sur le poulet');
+            console.log(error)
+        }
+    );
+
+}
+
+function loadboat() {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/boat/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            gltf.scene.traverse( function ( child ) {
+
+                if ( child.isMesh ) {
+
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+
+                }
+
+            } );
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene.scale.set(0.1,0.1,0.1); // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+
+            gltf.scene.rotation.y = 0;
+            gltf.scene.position.x = 0;
+            gltf.scene.position.z = 130;
+            gltf.scene.position.y = 12;
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded tree');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+function loadcrystal(Px,Py,Pz) {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/crystal/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene.scale.set(2,2,2); // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.rotation.y = 80;
+            gltf.scene.position.x = Px;
+            gltf.scene.position.z = Pz;
+            gltf.scene.position.y = Py;
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded house');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+function loadHouse() {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/house/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene.scale.set(1.5,1.5,1.5); // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.rotation.y =  90 * Math.PI / 180;
+            gltf.scene.position.x = 17;
+            gltf.scene.position.z = 72;
+            gltf.scene.position.y = 30.3;
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded house');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+function loader(objectName,Px,Py,Pz,scale) {
+    var loader = new GLTFLoader();
+    loader.load(
+        // Chemin de la ressource
+        './src/objects/'+objectName+'/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            gltf.scene.traverse( function ( child ) {
+                if ( child.isMesh ) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            } );
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene.scale.set(scale,scale,scale); // THREE.Scene
+            gltf.scenes; // Array<THREE.Scene>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.rotation.y = 0;
+            gltf.scene.position.x = Px;
+            gltf.scene.position.z = Pz;
+            gltf.scene.position.y = Py;
+        },
+
+        // Fonction appelée lors du chargement
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded tree');
+        },
+
+        // Fonction appelée lors d'une quelconque erreur
+        function (error) {
+            console.log('Une erreur est survenue');
+            console.log(error)
+        }
+    );
+
+}
+
+
+/**
+ * Fonction d'évenement, lancée en cas de resize de la fenêtre
+ */
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+/**
+ * Fonction d'animation du monde
+ */
+function animate() {
+    requestAnimationFrame( animate );
+    render();
+    stats.update();
+
+}
+
+/**
+ * Fonction de rendu
+ */
+function render() {
+    renderer.render(scene, camera);
+}
